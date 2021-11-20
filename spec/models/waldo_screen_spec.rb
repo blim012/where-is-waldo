5fe54491c04752bee991be7b6a9bd6b7ee1be4a8
Brandon Lim <brandonlimchi@gmail.com>
2021-11-20 02:27:29 -0800

    add scores association test to waldo_screen

--- spec/models/waldo_screen_spec.rb
+++ spec/models/waldo_screen_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe WaldoScreen, type: :model do
     expect(subject).to be_valid
   end
 
+  describe 'associations' do
+    it { should have_many(:scores) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:image_url) }
