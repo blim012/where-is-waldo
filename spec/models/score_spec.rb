8c74d804c79b507b7b33f78850647e48fef6af38
Brandon Lim <brandonlimchi@gmail.com>
2021-11-20 02:20:05 -0800

    add and test associations and validations for Score model

--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Score, type: :model do
-  
+  describe 'associations' do
+    it { should belong_to(:waldo_screen).required }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:seconds) }
+  end
 end
