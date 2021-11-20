2b964f54a48ec8b1ec913e02f8dbf4642ab65cb5
Brandon Lim <brandonlimchi@gmail.com>
2021-11-20 01:13:46 -0800

    install react-router-dom and create react router

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -4,23 +4,11 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
+import RouteSwitch from '../routers/RouteSwitch'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <RouteSwitch />,
     document.body.appendChild(document.createElement('div')),
   )
-})
+});
---
+++ app/javascript/routers/RouteSwitch.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const RouteSwitch = (props) => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RouteSwitch;
