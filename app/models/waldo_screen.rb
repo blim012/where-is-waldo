be56b27af5a1fb4c6d6ac94668a836d233e29f26
Brandon Lim <brandonlimchi@gmail.com>
2021-11-20 21:00:57 -0800

    refactor waldoscreen associations to have many positions instead of characters

--- app/models/waldo_screen.rb
+++ app/models/waldo_screen.rb
@@ -1,6 +1,6 @@
 class WaldoScreen < ApplicationRecord
   has_many :scores
-  has_many :characters
+  has_many :positions
   validates :name, presence: true
   validates :image_url, presence: true
 end
--- spec/models/waldo_screen_spec.rb
+++ spec/models/waldo_screen_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe WaldoScreen, type: :model do
 
   describe 'associations' do
     it { should have_many(:scores) }
-    it { should have_many(:characters) }
+    it { should have_many(:positions) }
   end
 
   describe 'validations' do
