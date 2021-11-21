0268d07b859f187253b84b684c15761970908977
Brandon Lim <brandonlimchi@gmail.com>
2021-11-20 22:24:24 -0800

    initial implementation of waldoscreen controller in api

---
+++ app/controllers/api/v1/waldo_screens_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::WaldoScreensController < ApplicationController
+  def index
+    @screens = WaldoScreen.all
+    render json: @screens
+  end
+
+  def show
+    @screen = WaldoScreen.find_by id: params[:id]
+    if @screen
+      render json: @screen
+    else
+      render json: {error: 'Waldo screen does not exist'}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :waldo_screens, only: [ :index, :show ]
+    end
+  end
+
   root to: 'waldo#index'
   get '*path', to: 'waldo#index'
 end
