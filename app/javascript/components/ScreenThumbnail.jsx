b8fa7e54e5f70955bec303146e4d4c4a4b8cb47e
Brandon Lim <brandonlimchi@gmail.com>
2021-11-22 00:51:40 -0800

    style link text to black

--- app/javascript/components/ScreenThumbnail.jsx
+++ app/javascript/components/ScreenThumbnail.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const ScreenThumbnail = (props) => {
   return (
-    <Link to={'/' + props.id}>
+    <Link to={'/' + props.id} style={{ color: 'black' }}>
       <div className="screen-thumbnail">
         <p className="screen-name">{props.name}</p>
         <img className="screen-image" src={require(`images/screens/${props.image_url}`)} alt={props.name} />
