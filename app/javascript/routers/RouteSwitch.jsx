bc08fc979c11b3bb5090e24308a02da29a99caae
Brandon Lim <brandonlimchi@gmail.com>
2021-11-22 00:35:06 -0800

    add route to Screen component

--- app/javascript/routers/RouteSwitch.jsx
+++ app/javascript/routers/RouteSwitch.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ScreenSelect from "../components/ScreenSelect";
+import Screen from "../components/Screen";
 
 const RouteSwitch = (props) => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ScreenSelect />} />
+        <Route exact path="/:screen_id" element={<Screen />} />
       </Routes>
     </BrowserRouter>
   );
