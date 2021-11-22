824e4c140c4e1aa10ad3f7295d6490ce66212614
Brandon Lim <brandonlimchi@gmail.com>
2021-11-22 01:21:54 -0800

    create seconds timer in Game component

--- app/javascript/components/Game.jsx
+++ app/javascript/components/Game.jsx
@@ -3,9 +3,28 @@ import { useParams } from "react-router";
 
 const Game = (props) => {
   const params = useParams();
+  const [seconds, setSeconds] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if(loaded) {
+      interval = setInterval(() => {
+        setSeconds(seconds + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [loaded, seconds]);
 
   return (
-    <div><br /><br /><br />Game Screen {params.screen_id}</div>
+    <div>
+      <br /><br /><br />
+      <p>Game Screen {params.screen_id}</p>
+      <p>Seconds Elapsed: {seconds}</p>
+      <button onClick={() => setLoaded(true)}>Start Time</button>
+      <button onClick={() => setLoaded(false)}>Stop Time</button>
+    </div>
   );
 };
 
