c2d85eef6b92519858e7b4e433868669bd3da86d
Brandon Lim <brandonlimchi@gmail.com>
2021-11-22 22:09:59 -0800

    style crosshair for dropdown list

--- app/javascript/components/Screen.jsx
+++ app/javascript/components/Screen.jsx
@@ -21,14 +21,20 @@ const Screen = (props) => {
   return (
     <div id="game-screen" onClick={handleDropDown}>
       <div className="dropdown no-display">
-        {Object.keys(icons).map((key) => {
-          return (
-            <div className="dropdown-item" key={uniqid('dropdown-')}>
-              <img src={icons[key]} />
-              <p>{key}</p>
-            </div>
-          )
-        })}
+        <div className="dropdown-rel-wrapper">
+          <div className="crosshair crosshair-inner"></div>
+          <div className="crosshair crosshair-border"></div>
+          <div className="dropdown-list">
+            {Object.keys(icons).map((key) => {
+              return (
+                <div className="dropdown-item" key={uniqid('dropdown-')}>
+                  <img src={icons[key]} />
+                  <p>{key}</p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
       <img id="screen" src={image_url} alt="Oops, something went wrong! Game screen cannot load." />
     </div>
