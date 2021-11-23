a68ba6de0a0c44afd85a87827125534cdcab35ba
Brandon Lim <brandonlimchi@gmail.com>
2021-11-22 18:22:51 -0800

    fetch and store character positions and icons

--- app/javascript/components/Game.jsx
+++ app/javascript/components/Game.jsx
@@ -1,10 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import axios from "axios";
 
 const Game = (props) => {
   const params = useParams();
   const [seconds, setSeconds] = useState(0);
   const [loaded, setLoaded] = useState(false);
+  const [positions, setPositions] = useState({});
+  const [icons, setIcons] = useState({});
+  const [screen, setScreen] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/v1/waldo_screens/${params.screen_id}`)
+    .then((response) => {
+      let data = response.data;
+      let positionsToSet = {};
+      let iconsToSet = {};
+      data.positions.forEach((position) => {
+        let iconSrc = require(`images/characters/${position.character.image_url}`);
+        iconsToSet[position.character.name] = iconSrc;
+        positionsToSet[position.character.name] = {x_pos: position.x_pos, y_pos: position.y_pos};
+      });
+
+      setPositions(positionsToSet);
+      setIcons(iconsToSet);
+    })
+    .catch((error) => {
+      // Catch error here
+    });
+  }, []);
 
   useEffect(() => {
     let interval = null;
