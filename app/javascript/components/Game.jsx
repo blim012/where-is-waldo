7d4e78674a5cfa04ee9181aa57503f9923f7093c
Brandon Lim <brandonlimchi@gmail.com>
2021-11-23 22:07:14 -0800

    detect if player has won

--- app/javascript/components/Game.jsx
+++ app/javascript/components/Game.jsx
@@ -10,6 +10,7 @@ const Game = (props) => {
   const [positions, setPositions] = useState({});
   const [icons, setIcons] = useState({});
   const [screen, setScreen] = useState(null);
+  const [found, setFound] = useState({Waldo: false, Wenda: false, Odlaw: false, Wizard: false});
 
   useEffect(() => {
     axios.get(`/api/v1/waldo_screens/${params.screen_id}`)
@@ -44,7 +45,14 @@ const Game = (props) => {
     return () => clearInterval(interval);
   }, [loaded, seconds]);
 
+  useEffect(() => {
+    if(checkWin()) {
+      handleWin();
+    }
+  }, [found]);
+
   const checkSelection = (character, xPercentage, yPercentage) => {
+    if(found[character]) return false; // Already found character
     const errorMargin = 0.03;
     const upperRangeX = positions[character].x_pos + errorMargin;
     const lowerRangeX = positions[character].x_pos - errorMargin;
@@ -53,8 +61,25 @@ const Game = (props) => {
     if((xPercentage <= upperRangeX && xPercentage >= lowerRangeX) &&
        (yPercentage <= upperRangeY && yPercentage >= lowerRangeY)) {
       console.log(`${character} has been found`);
+      let foundToSet = {...found};
+      foundToSet[character] = true;
+      setFound(foundToSet);
+      return true;
     }
+    return false;
   };
+
+  const checkWin = () => {
+    const foundList = Object.values(found);
+    for(let i = 0; i < foundList.length; i++) {
+      if(!foundList[i]) return false;
+    }
+    return true;
+  }
+
+  const handleWin = () => {
+    console.log('You win');
+  }
 
   return (
     <div id="game">
