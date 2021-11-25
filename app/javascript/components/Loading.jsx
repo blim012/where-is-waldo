818600af8fae890954117e1635d183f4b10dbe2c
Brandon Lim <brandonlimchi@gmail.com>
2021-11-25 01:02:33 -0800

    create Loading component

---
+++ app/javascript/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <p className="loading">Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
