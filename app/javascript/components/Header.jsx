d57e6156383c0a575ccc93f898593d9ae4fb6cfd
Brandon Lim <brandonlimchi@gmail.com>
2021-11-25 00:24:57 -0800

    create header component

---
+++ app/javascript/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = (props) => {
+  const headerText = props.headerText;
+
+  return (
+    <header className="header">
+      <Link to="/" className="home-link header-item">
+        <p>Where's Waldo</p>
+      </Link>
+      {headerText && headerText.map((text) => {
+        <div className="header-text header-item">
+          <p>{text}</p>
+        </div>
+      })}
+    </header>
+  )
+}
+
+export default Header;
